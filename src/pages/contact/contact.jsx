f2fdc6d26fd73b679844c453b139dca060a47e49
SasithDuleepa <duleepajayathissa@gmail.com>
2023-04-05 00:37:37 +0530

    upload

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -5,14 +5,15 @@ import { SlSocialFacebook,SlSocialGithub,SlSocialLinkedin,SlEnvolope,SlPhone,SlE
 export default function Contact() {
   return (
     <div className='contact_main'>
+      <div className='contact_frame'>
       <p className='contact_me'>Contact me...</p>
       <div className='contactme'>mobile no. : <a> 077 9090516</a></div>
-      <div className='contactme'>e-mail address : <a> sasithduleepa@gmail.com</a></div>
-      
+      <div className='contactme'>e-mail : <a> sasithduleepa@gmail.com</a></div>
+      </div>
      
       <div className='social_frame'>
       <p className='follow_me'>Follow me...</p>
-      <div className='socialacc'><SlSocialLinkedin/> <a href='www.linkedin.com/in/sasith-duleepa'>linked in</a> </div>
+      <div className='socialacc'><SlSocialLinkedin/> <a href='www.linkedin.com/in/sasith-duleepa'>linkedin</a> </div>
       <div className='socialacc'><SlSocialGithub/> <a href='https://github.com/SasithDuleepa'>github</a> </div>
       <div className='socialacc'><SlSocialFacebook/> <a href=''>facebook</a> </div>
       </div>
